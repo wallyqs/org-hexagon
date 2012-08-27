f3c497bd4948799f1efb360166f2c344ab850aea
Waldemar Quevedo <waldemar.quevedo@gmail.com>
2012-08-27 23:59:10 +0900

    Client hello world

--- lisp/org-hexagon.el
+++ lisp/org-hexagon.el
@@ -1,19 +1,29 @@
 ;; Client for the API from OrgHexagon
 (require 'url)
+(require 'json)
 
-(setq org-hexagon-url "http://127.0.0.1:9393/api/text.json")
+(setq org-hexagon-url "http://127.0.0.1:9393/")
+(setq org-hexagon-api-url "http://127.0.0.1:9393/api/")
 
-(defun org-hexagon-get-text ()
-  (interactive)
+(defun org-hexagon-put-text (beg end)
+  (interactive "r")
+
   (let* ((url-request-method "POST")
+         (org-text 
+	  (buffer-substring-no-properties beg end))
          (url-request-extra-headers '(("Content-Type" . "application/json")))
          (url-request-data
-          (encode-coding-string (concat "{"
-                                        "\"content\": \"* HELLO WORLD\""
-                                        "}"
-                                        ) 'utf-8))
-         )
-    (url-retrieve org-hexagon-url
+          (encode-coding-string
+           (json-encode
+            `((content . ,org-text))
+            ) 'utf-8)
+          )
+	 (uri (concat 
+	       org-hexagon-api-url 
+	       "texts.json"))
+         )                               ;end of let varlist
+
+    (url-retrieve uri
                   '(lambda (status)
                      (switch-to-buffer (current-buffer))
                      ))
