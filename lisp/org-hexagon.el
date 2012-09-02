410801872be0b1cf79762d08c71992a563a189a0
Waldemar Quevedo <waldemar.quevedo@gmail.com>
2012-09-02 22:34:25 +0900

    Add simple emacs client for the service

--- lisp/org-hexagon.el
+++ lisp/org-hexagon.el
@@ -2,30 +2,66 @@
 (require 'url)
 (require 'json)
 
+(defvar url-http-end-of-headers)
+(defvar url-http-response-status)
+
 (setq org-hexagon-url "http://127.0.0.1:9393/")
 (setq org-hexagon-api-url "http://127.0.0.1:9393/api/")
 
-(defun org-hexagon-put-text (beg end)
+(defun org-hexagon-request (method url &optional data)
+  "Returns a JSON object with the contents from the request"
+
+  (let* ((url-request-extra-headers '(("Content-Type" . "application/json")))
+         (url-request-method method)
+         (url-request-data
+          (if data (encode-coding-string data 'utf-8)))
+         (buf))
+
+    (setq buf (url-retrieve-synchronously url))
+    (with-current-buffer buf
+      (goto-char url-http-end-of-headers)
+      (prog1 (json-read)
+        (kill-buffer))
+      )))
+
+(defun org-hexagon-all-texts ()
+  "Creates a buffer with all the texts so far merged"
+  (interactive)
+
+  (let* ((json
+          (org-hexagon-request
+           "GET" (concat org-hexagon-api-url "texts.json")))
+         (buf (get-buffer-create "*Org Hexagon Texts*")))
+
+    (with-current-buffer buf
+      (mapcar (lambda(org-text)
+                (insert
+                 (concat
+                  (cdr
+                   (assoc 'content org-text)) "\n"))
+                ) json))
+
+    (switch-to-buffer buf)
+    (org-mode)))
+
+(defun org-hexagon-sync-text-region(beg end)
+  "Create org-mode text from region"
   (interactive "r")
 
-  (let* ((url-request-method "POST")
-         (org-text 
-	  (buffer-substring-no-properties beg end))
-         (url-request-extra-headers '(("Content-Type" . "application/json")))
-         (url-request-data
-          (encode-coding-string
-           (json-encode
-            `((content . ,org-text))
-            ) 'utf-8)
-          )
-	 (uri (concat 
-	       org-hexagon-api-url 
-	       "texts.json"))
-         )                               ;end of let varlist
+  (let* ((region
+          (buffer-substring-no-properties beg end))
+         (org-text
+          (json-encode
+           `((content . ,region))))
+         (url (concat org-hexagon-api-url "texts.json"))
+         (response
+          (org-hexagon-request
+           "POST" url org-text))
+         (status (cdr (assoc 'status response))))
 
-    (url-retrieve uri
-                  '(lambda (status)
-                     (switch-to-buffer (current-buffer))
-                     ))
+    (if (equal status 200)
+	(message (concat "Org text created with id: " (cdr (assoc '_id response))))
+      (message "Not saved"))
     )
   )
+
